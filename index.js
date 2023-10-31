1a01da4f07d003c2770a9e2343639422819a1b55
mozzam123 <mozzam607@gmail.com>
2023-10-31 11:44:57 +0530

    added index file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
+const path = require("path")
+
 
 
 // Serve static files from public directory
 app.use(express.static("public"))
 
-
+// Set view engine and template path
+app.set("view engine", "hbs");
+app.set("views", template_path);
+app.set("views", path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
     res.send("mozzam Inamdar")
