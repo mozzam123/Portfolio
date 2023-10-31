bf84ed531aae09258ff4828c3d960933469ebf13
mozzam123 <mozzam607@gmail.com>
2023-10-31 11:33:53 +0530

    made structure for the app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("mozzam Inamdar")
+})
+
+
+app.listen(7900, () => {
+    console.log('Server running on 7900');
+})
